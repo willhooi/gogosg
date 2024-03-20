b41dc0e34c55c57dadc9079461aa500eac896ca0
William Hooi <william.hooi@gmail.com>
2024-03-20 20:50:17 +0800

    get data using STB API and display it as table

--- ui/server.js
+++ ui/server.js
@@ -1,10 +1,17 @@
 const fs = require('fs');
 const express = require('express');
+const { ApolloServer} = require('apollo-server-express');
+
+//write resolver
+//start GQL server
+//STB API URL: https://api.stb.gov.sg/content/food-beverages/v2/search
+//API key:gS8i7oE7GLfMLZnnA0tZOwXTNSDgPqwB
+//params: searchType=keyword&searchValues=economy%20rice
 
 
+//Start Express server
 const app = express();
 app.use(express.static('public'));
-
 
 ( function () {
   try {
