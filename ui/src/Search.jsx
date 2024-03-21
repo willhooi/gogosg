51d2947c3e8e42b3f9869d90ae5fb4b66fa32600
William Hooi <william.hooi@gmail.com>
2024-03-21 19:15:23 +0800

    fixing search

--- ui/src/Search.jsx
+++ ui/src/Search.jsx
@@ -1,12 +1,57 @@
+import ShowPlaces from "./ShowPlaces.jsx";
+
 export default class Search extends React.Component {
 	constructor() {
 	super();
+	this.state = {searchplaces: []};
+    this.handleSubmit = this.handleSubmit.bind(this);
 	}
 
-    render(){
-    return (
-        <div>
-            <h5>Placeholder for Search</h5>
-        </div>);
+    handleSubmit(e) {
+        e.preventDefault();
+        const form = document.forms.searchItem;
+        const searchplace = {
+          name: form.searchitem.value,
+        }
+        this.searchplaces(searchplace);
+        form.searchitem.value = "";
+      }
+
+	async searchplaces(args){
+        console.log(args);
+
+		const response = await fetch('https://api.stb.gov.sg/content/food-beverages/v2/search?searchType=keyword&searchValues='+args.name+'&sort=name&sortOrder=asc',{
+			method: 'GET',
+      		headers: { 
+				'Content-Type': 'application/json',
+				'Host': 'api.stb.gov.sg',
+				'Accept': 'application/json',
+				'X-Content-Language':'en',
+				'X-API-Key':'gS8i7oE7GLfMLZnnA0tZOwXTNSDgPqwB',
+				},
+		});
+		const data = await response.json();	
+		this.setState({searchplaces : data.data });
+		console.log(this.state.searchplaces);
+		};
+
+
+	render(){
+	return (
+	<div className="container m-2">
+		<h5>Placeholder for Search</h5>
+		
+		<div className="row m-2">
+            <div> 
+                <form name="searchItem" onSubmit={this.handleSubmit}>
+                    <input type="text" name="searchitem" className="form-control" placeholder="Search Item" required/>
+                    <button className="btn btn-secondary m-2">Search</button>
+                </form>
+            </div>
+            <div><ShowPlaces places={this.state.searchplaces} /></div>
+				
+		</div>
+
+	</div>);
 	}
 }
